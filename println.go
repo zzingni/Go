440adba9f2ad177800d5f39b99721800f6d21249
zzingni <wldms001313@gmail.com>
2023-10-11 21:51:43 +0900

    v0.1 first practice

--- println.go
+++ println.go
@@ -8,5 +8,8 @@ import (
 
 func main() {
 	fmt.Println(math.Floor(2.75))
+	fmt.Println(math.Ceil(2.75))
+	fmt.Println(math.Sqrt(16))
+	fmt.Println("Opensource\tprogramming\n\"go!\"")
 	fmt.Println(strings.Title("head first go"))
 }
