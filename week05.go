3148d4835e810e7eaf11774fa1e19e3c08a017ad
zzingni <wldms001313@gmail.com>
2023-10-12 00:25:16 +0900

    v0.2 Replacer

--- week05.go
+++ week05.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
 func main() {
+
+	Hotspurs := "hm ? j? madi?"
+	replacePlayer := strings.NewReplacer("?", "son") // 'Replacer' 타입의 값을 생성하고, 'Replacer' 객체를 반환함.
+	player := replacePlayer.Replace(Hotspurs)
+	fmt.Println(player)
+
 	var now time.Time
 	now = time.Now()
 	var year int = now.Year()
