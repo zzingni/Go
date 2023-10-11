7ef877be46cb3bbe1884fc688a1ef473b9efd71a
zzingni <wldms001313@gmail.com>
2023-10-12 01:43:21 +0900

    0.5 rand()_2

--- week05.go
+++ week05.go
@@ -12,8 +12,34 @@ import (
 
 func main() {
 
-	dice := rand.Intn(6) + 1 // 1~6까지 랜덤한 수 출력
-	fmt.Println(dice)
+	fmt.Println("Guess number game!")
+	answer := rand.Intn(100) + 1 // 1~100
+	fmt.Println(answer)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("Chance left : ", 10-i)
+		fmt.Print("Input number : ")
+		reader := bufio.NewReader(os.Stdin)
+		inputGuessString, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		inputGuessString = strings.TrimSpace(inputGuessString)
+		inputGuess, err := strconv.Atoi(inputGuessString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if inputGuess == answer {
+			fmt.Println("Great! You got the number, Congraturation~ ")
+		} else if inputGuess < answer {
+			fmt.Println("Your guess number is lower than answer! ")
+		} else if inputGuess > answer {
+			fmt.Println("Your guess number is higher than answer! ")
+		}
+	}
+
+	/* 	dice := rand.Intn(6) + 1 // 1~6까지 랜덤한 수 출력
+	   	fmt.Println(dice) */
 
 	fmt.Print("input Score : ")
 	reader := bufio.NewReader(os.Stdin)
